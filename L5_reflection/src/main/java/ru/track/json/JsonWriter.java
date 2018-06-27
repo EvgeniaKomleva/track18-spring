aa8369f4f67bb808b2cddcce8536e87005b46a05
Evgenia Komleva <komleva.1999@inbox.ru>
2018-06-28 01:12:42 +0300

    json

--- L5_reflection/src/main/java/ru/track/json/JsonWriter.java
+++ L5_reflection/src/main/java/ru/track/json/JsonWriter.java
@@ -3,11 +3,13 @@ package ru.track.json;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.lang.annotation.Annotation;
 
 
 /**
@@ -19,34 +21,42 @@ public class JsonWriter {
     public static String toJson(@Nullable Object object) {
         if (object == null) {
             return "null";
+
         }
 
         Class clazz = object.getClass();
 
+
         if (clazz.equals(String.class)
                 || clazz.equals(Character.class)
                 || clazz.isEnum()
+
                 ) {
             return String.format("\"%s\"", object);
         }
 
         if (object instanceof Boolean || object instanceof Number) {
             return object.toString();
+
         }
 
         if (clazz.isArray()) {
             return toJsonArray(object);
+
         }
 
         if (object instanceof Collection) {
             return toJsonCollection(object);
+
         }
 
         if (object instanceof Map) {
             return toJsonMap(object);
+
         }
 
         return toJsonObject(object);
+
     }
 
     /**
@@ -60,9 +70,26 @@ public class JsonWriter {
     @NotNull
     private static String toJsonArray(@NotNull Object object) {
         int length = Array.getLength(object);
-        // TODO: implement!
+        Object item;
+        StringBuilder result = new StringBuilder();
+        result.append('[');
 
-        return null;
+
+        for (int i = 0; i < length; ++i) {
+            item = Array.get(object, i);
+            result.append(toJson(item));
+
+            if (i < length - 1) {
+                result.append(",");
+
+            }
+        }
+
+        result.append(']');
+
+
+        return result.toString();
+
     }
 
     /**
@@ -70,9 +97,11 @@ public class JsonWriter {
      */
     @NotNull
     private static String toJsonCollection(@NotNull Object object) {
+
         Collection collection = (Collection) object;
         return toJsonArray(collection.toArray());
     }
+
 
     /**
      * Сконвертить мап в json. Формат:
@@ -82,9 +111,19 @@ public class JsonWriter {
      */
     @NotNull
     private static String toJsonMap(@NotNull Object object) {
-        // TODO: implement!
 
-        return null;
+        Map<String, String> stringMap = new LinkedHashMap<>();
+        Map<Object, Object> map = (Map) object;
+
+
+        for (Map.Entry<Object, Object> entry : map.entrySet()) {
+            Object key = entry.getKey();
+
+            stringMap.put(String.valueOf(key), toJson(entry.getValue()));
+        }
+
+
+        return formatObject(stringMap);
         // Можно воспользоваться этим методом, если сохранить все поля в новой мапе уже в строковом представлении
 //        return formatObject(stringMap);
     }
@@ -107,11 +146,54 @@ public class JsonWriter {
      */
     @NotNull
     private static String toJsonObject(@NotNull Object object) {
+        Map<String, String> map = new LinkedHashMap<>();
+
+
         Class clazz = object.getClass();
-        // TODO: implement!
 
 
-        return null;
+        boolean nullableObject = false;
+        Annotation jsonNullable = clazz.getAnnotation(JsonNullable.class);
+        if (jsonNullable != null && jsonNullable.annotationType().getSimpleName().equals("JsonNullable")) {
+
+            nullableObject = true;
+        }
+
+        Field[] fields = clazz.getDeclaredFields();
+        Object obj;
+
+        for (Field field : fields) {
+            String fieldName;
+            field.setAccessible(true);
+            SerializedTo serializedTo = field.getAnnotation(SerializedTo.class);
+            if (serializedTo != null) {
+
+                fieldName = serializedTo.value();
+            }
+            else{
+                fieldName = field.getName();
+
+            }
+
+            try {
+                obj = field.get(object);
+
+            }
+            catch (IllegalAccessException e) {
+                continue;
+
+            }
+
+            if (obj == null && !nullableObject) {
+                continue;
+
+            }
+            map.put(fieldName, toJson(obj));
+
+        }
+
+
+        return formatObject(map);
     }
 
     /**
@@ -122,12 +204,15 @@ public class JsonWriter {
      */
     @NotNull
     private static String formatObject(@NotNull Map<String, String> map) {
+
         String r = String.join(",", map.entrySet().stream()
                 .map(e -> String.format("\"%s\":%s", e.getKey(), e.getValue()))
                 .collect(Collectors.toList())
+
         );
 
         return String.format("{%s}", r);
+
     }
 
 }
