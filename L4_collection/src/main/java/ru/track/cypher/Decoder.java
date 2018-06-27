62ae4de5dc1104299d96243754b5b3355519cf94
Evgenia Komleva <komleva.1999@inbox.ru>
2018-06-28 01:15:54 +0300

    cypher

--- L4_collection/src/main/java/ru/track/cypher/Decoder.java
+++ L4_collection/src/main/java/ru/track/cypher/Decoder.java
@@ -6,12 +6,9 @@ import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 public class Decoder {
-
     // Расстояние между A-Z -> a-z
     public static final int SYMBOL_DIST = 32;
-
     private Map<Character, Character> cypher;
-
     /**
      * Конструктор строит гистограммы открытого домена и зашифрованного домена
      * Сортирует буквы в соответствие с их частотой и создает обратный шифр Map<Character, Character>
@@ -21,27 +18,15 @@ public class Decoder {
     public Decoder(@NotNull String domain, @NotNull String encryptedDomain) {
         Map<Character, Integer> domainHist = createHist(domain);
         Map<Character, Integer> encryptedDomainHist = createHist(encryptedDomain);
-
         cypher = new LinkedHashMap<>();
-
-
     }
-
     public Map<Character, Character> getCypher() {
         return cypher;
     }
-
-    /**
-     * Применяет построенный шифр для расшифровки текста
-     *
-     * @param encoded зашифрованный текст
-     * @return расшифровка
-     */
     @NotNull
     public String decode(@NotNull String encoded) {
         return null;
     }
-
     /**
      * Считывает входной текст посимвольно, буквы сохраняет в мапу.
      * Большие буквы приводит к маленьким
@@ -52,6 +37,7 @@ public class Decoder {
      * Мапа отсортирована по частоте. При итерировании на первой позиции наиболее частая буква
      */
     @NotNull
+
     Map<Character, Integer> createHist(@NotNull String text) {
         return null;
     }
--- L4_collection/src/main/java/ru/track/cypher/Encoder.java
+++ L4_collection/src/main/java/ru/track/cypher/Encoder.java
@@ -21,6 +21,7 @@ public class Encoder {
      * @return зашифрованный текст
      */
     public String encode(@NotNull Map<Character, Character> cypherTable, @NotNull String text) {
+
         return null;
     }
 }
