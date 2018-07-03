6b3b9a4e03658f08820f85d8cecb5a0b5b6e2f76
Evgenia Komleva <komleva.1999@inbox.ru>
2018-07-03 13:53:29 +0300

    words

--- L2-objects/src/main/java/ru/track/CountWords.java
+++ L2-objects/src/main/java/ru/track/CountWords.java
@@ -1,59 +1,101 @@
 package ru.track;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 
 /**
- * Задание 1: Реализовать два метода
- *
- * Формат файла: текстовый, на каждой его строке есть (или/или)
- * - целое число (int)
- * - текстовая строка
- * - пустая строка (пробелы)
- *
- * Числа складываем, строки соединяем через пробел, пустые строки пропускаем
- *
- *
- * Пример файла - words.txt в корне проекта
- *
- * ******************************************************************************************
- *  Пожалуйста, не меняйте сигнатуры методов! (название, аргументы, возвращаемое значение)
- *
- *  Можно дописывать новый код - вспомогательные методы, конструкторы, поля
- *
- * ******************************************************************************************
- *
+ @@ -26,9 +28,10 @@
  */
 public class CountWords {
 
     String skipWord;
+    String skipWord = "vu53f28MvpQ4PclHvxHZ";
 
     public CountWords(String skipWord) {
-        this.skipWord = skipWord;
-    }
+            public CountWords(String skipWord)
+                    {
+                this.skipWord = skipWord; }
 
-    /**
-     * Метод на вход принимает объект File, изначально сумма = 0
-     * Нужно пройти по всем строкам файла, и если в строке стоит целое число,
-     * то надо добавить это число к сумме
-     * @param file - файл с данными
-     * @return - целое число - сумма всех чисел из файла
-     */
-    public long countNumbers(File file) throws Exception {
-        return 0;
-    }
+        @@ -39,8 +42,27 @@
+ public CountWords(String skipWord) {
+      /** @param file - файл с данными
+      * @return - целое число - сумма всех чисел из файла
+      */
+                public long countNumbers(File file) throws Exception {
+                        return 0;
+                    public long countNumbers(File file) throws Exception
+                            {
+                                FileReader fileReader = new FileReader (file);
+                        BufferedReader bufferedReader = new BufferedReader(fileReader);
+                        String line = null;
+                        long sum = 0;
+                        while (true)
+                            {
+                                        line = bufferedReader.readLine();
+                            if (line == null)
+                                {
+                                            break;
+                            }
+                            try {
+                                    sum += Integer.parseInt(line);
+                                } catch (NumberFormatException e)
+                            {
+                                    }
+
+                                }
+                        return sum;
+     }
 
 
-    /**
-     * Метод на вход принимает объект File, изначально результат= ""
-     * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
-     * то надо присоединить ее к результату через пробел
-     * @param file - файл с данными
-     * @return - результирующая строка
-     */
-    public String concatWords(File file) throws Exception {
-        return null;
-    }
+                @@ -51,8 +73,48 @@ public long countNumbers(File file) throws Exception {
+     /* * @param file - файл с данными
+      * @return - результирующая строка
+                            */
 
-}
+                                public static void main (String[] args) throws Exception{
+                                CountWords cw = new CountWords("");
+                                File src = new File ("C:\\Users\\haker\\Desktop\\технотрек\\java\\track18-spring\\L2-objects\\words.txt");
+                                System.out.println(cw.concatWords(src));
+                            }
 
+                    public String concatWords(File file) throws Exception {
+                                return null;
+                                StringBuilder result = new StringBuilder("");
+                                FileReader fileReader = new FileReader (file);
+                                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                                String line = "";
+                                boolean num = false;
+                                boolean empty = false;
+                                //1
+                                        while (true)
+                                    {
+                                                line = bufferedReader.readLine();
+                                    if (line == null)
+                                        {
+                                                    break;
+                                    }
+                                    if ((line.equals("")) || (line.equals(skipWord)))
+                                        {
+                                                    empty = true;
+                                    }
+                                    try {
+                                            Integer.parseInt(line);
+                                        } catch (NumberFormatException e)
+                                    {
+                                                num = true;
+                                    }
+
+                                            if ((num != false) &&(empty == false))
+                                        {
+                                                    result.append(line);
+                                        result.append(" ");
+                                    }
+                                    num = false;
+                                    empty = false;
+                                }
+                                return result.toString();
+                    }
+
+                }
